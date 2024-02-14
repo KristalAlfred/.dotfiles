ae7f3c3759ddea04fe4bb58f21727960f2e6c09c
Alfred Kristal Ern <alfred.kristal@gmail.com>
2024-02-14 21:05:28 +0100

    add function to pick image id and to remove images

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Visually select containers
 dsel() {
 docker ps --format "{{.Names}}\t{{.Image}}\t{{.ID}}\t{{.CreatedAt}}" | awk '
@@ -33,4 +35,61 @@ denter() {
 # View logs from a selected container
 dlog() {
     did | xargs -I {} docker logs -f {}
+}
+
+dimgid() {
+  docker images --format "{{.Repository}}\t{{.Tag}}\t{{.Size}}\t{{.ID}}\t{{.CreatedAt}}" | awk '
+    {
+      # Adjust the output format here
+      printf("%-30s\t%-20s\t%s\t%s\n", $1, $2, $3, $4 " " $5 " " $6);
+    }' | fzf | awk '{ print $4 }'
+}
+
+# Remove selected image together with its containers
+dimgrm() {
+  local img_id=$(dimgid)
+
+  if [[ -z "$img_id" ]]; then
+    echo "No image selected."
+    return
+  fi
+
+  local containers_using=$(docker ps -a --filter "ancestor=$img_id" --format "{{.ID}}\t{{.Names}}")
+
+  if [[ -n "$containers_using" ]]; then
+    echo "The following container(s) are using the image $img_id:"
+    echo "$containers_using"
+    echo -n "Would you like to force delete this image and its containers? [y/N] "
+
+    read response
+
+    if [[ "$response" =~ ^[Yy]$ ]]; then
+      echo "Stopping and removing containers..."
+      echo "$containers_using" | awk '{print $1}' | xargs -r docker rm -f
+      echo "Removing image $img_id..."
+      docker rmi -f "$img_id"
+    else
+      return
+    fi
+  else
+    if ! output=$(docker rmi "$img_id" 2>&1); then
+      echo "Failed to remove image: $img_id"
+      echo "$output"
+      echo -n "Would you like to force remove this image? [y/N] "
+      
+      read force_response
+
+      if [[ "$force_response" =~ ^[Yy]$ ]]; then
+        if docker rmi -f "$img_id"; then
+          echo "Successfully forced removal of image: $img_id"
+        else
+          echo "Failed to force remove image: $img_id"
+        fi
+      else
+        echo "Not forcing removal of image: $img_id"
+      fi
+    else
+      echo "Successfully removed image: $img_id"
+    fi
+  fi
 }
