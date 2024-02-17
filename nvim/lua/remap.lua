103991cf8e1408a367417526f329515c966d72fe
Alfred Kristal Ern <alfred.kristal@gmail.com>
2024-02-17 11:39:39 +0100

    add treesitter object keymap

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -101,8 +101,13 @@ vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>hh', builtin.help_tags, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ")});
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+-- ####
+-- Treesitter textobjects
+-- ####
+require('nvim-treesitter-textobjects')
 
 -- ####
 -- Treesitter
@@ -114,36 +119,78 @@ if vim.g.vscode then
 end
 
 require('nvim-treesitter.configs').setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {"vue", "javascript", "typescript", "rust", "c", "lua", "vim", "vimdoc", "query"},
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { "vue", "javascript", "typescript", "rust", "c", "lua", "vim", "vimdoc", "query" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-  highlight = {
-    enable = not isInVSCode,
+    highlight = {
+        enable = not isInVSCode,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+                ["as"] = "@block.outer", -- s for scope
+                ["is"] = "@block.inner",
+                ["rh"] = "@assignment.rhs",
+                ["lh"] = "@assignment.lhs",
+                ["ia"] = "@assignment.outer", -- assignment inner behaves weirdly imo
+                ["iq"] = "@parameter.inner", -- assignment inner behaves weirdly imo
+            }
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>sp"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>sP"] = "@parameter.inner",
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["mp"] = "@parameter.inner"
+            }
+        }
+    }
 }
+
+local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+-- Repeat movement with ; and ,
+-- ensure ; goes forward and , goes backward regardless of the last direction
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+-- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
 
 -- ####
 -- Undo Tree
 -- ####
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-
--- ####
--- Treesitter textobjects
--- ####
-require('nvim-treesitter-textobjects')
